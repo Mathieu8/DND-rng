71c2b2e48e420e10941d60bece81dd39fa10c83d
Mathieu <mathieuvanommeren@gmail.com>
2018-08-19 22:46:21 +0200

    first
    first

--- Calander/calander/Moons.java
+++ Calander/calander/Moons.java
@@ -8,78 +8,43 @@ public class Moons {
 	int periodW = 15; // 2 3 5 7 11
 	int periodA = 17; // 2 3 5 7 11 17
 	int periodE = 19; // 2 3 5 7 11 17 19
+	int period;
 
-	int phaseN;
-	int phaseP;
-	int phaseF;
-	int phaseW;
-	int phaseA;
-	int phaseE;
+	int phase;
+	String p;
 
-	public Moons(Calander c) {
+	public Moons(int period, int daysFromyearZero, Calander c) {
 		this.c = c;
-		phaseN = c.getDaysFromYearZero() % periodN;
-		phaseP = c.getDaysFromYearZero() % periodP;
-		phaseF = c.getDaysFromYearZero() % periodF;
-		phaseW = c.getDaysFromYearZero() % periodW;
-		phaseA = c.getDaysFromYearZero() % periodA;
-		phaseE = c.getDaysFromYearZero() % periodE;
+		this.period = period;
+		phase = c.getDaysFromYearZero() % period;
 
 	}
 
 	public void newDay(int n) {
 		if (n >= 0) {
-			phaseN = (phaseN + n) % periodN;
-			phaseP = (phaseP + n) % periodP;
-			phaseF = (phaseF + n) % periodF;
-			phaseW = (phaseW + n) % periodW;
-			phaseA = (phaseA + n) % periodA;
-			phaseE = (phaseE + n) % periodE;
-
+			phase = (phase + n) % period;
+			if (phase < period / 8) {
+				p = "NEW_MOON";
+			} else if (phase < 2 * period / 8) {
+				p = "WAXING_CRESCENT";
+			} else if (phase < 3 * period / 8) {
+				p = "FIRST_QUARTER";
+			} else if (phase < 4 * period / 8) {
+				p = "WAXING_GIBBOUS";
+			} else if (phase < 5 * period / 8) {
+				p = "FULL_MOON";
+			} else if (phase < 6 * period / 8) {
+				p = "WANING_GIBBOUS";
+			} else if (phase < 7 * period / 8) {
+				p = "THIRD_QUARTER";
+			} else {
+				p = "WANING_CRESCENT";
+			}
 		}
 	}
 
-	public void printMoonN() {
+	public void printMoon() {
 		System.out.print("negative moon is");
-		if (phaseN < periodN / 8) {
-			System.out.print("new  moon");
-		} else if (phaseN < 2 * periodN / 8) {
-			System.out.print("Waxing crescent");
-		} else if (phaseN < 3 * periodN / 8) {
-			System.out.print("First quarter");
-		} else if (phaseN < 4 * periodN / 8) {
-			System.out.print("Waxing gibbous");
-		} else if (phaseN < 5 * periodN / 8) {
-			System.out.print("Full moon");
-		} else if (phaseN < 6 * periodN / 8) {
-			System.out.print("Waning gibbous");
-		} else if (phaseN < 7 * periodN / 8) {
-			System.out.print("Third quarter ");
-		} else if (phaseN < 8 * periodN / 8) {
-			System.out.print("Waning crescent");
-		}
-		System.out.println();
-	}
-
-	public void printMoonF() {
-		System.out.print("Fire moon is ");
-		if (phaseF < periodF / 8) {
-			System.out.print("new  moon");
-		} else if (phaseF < 2 * periodF / 8) {
-			System.out.print("Waxing crescent");
-		} else if (phaseF < 3 * periodF / 8) {
-			System.out.print("First quarter");
-		} else if (phaseF < 4 * periodF / 8) {
-			System.out.print("Waxing gibbous");
-		} else if (phaseF < 5 * periodF / 8) {
-			System.out.print("Full moon");
-		} else if (phaseF < 6 * periodF / 8) {
-			System.out.print("Waning gibbous");
-		} else if (phaseF < 7 * periodF / 8) {
-			System.out.print("Third quarter ");
-		} else if (phaseF < 8 * periodF / 8) {
-			System.out.print("Waning crescent");
-		}
 		System.out.println();
 	}
 
